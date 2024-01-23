504d7e18cd9f1abc9a485c6fb741039e1ab983b8
Oleksandr Podustov <kydoter@gmail.com>
2024-01-23 19:10:57 +0200

    adapted unused method

--- main.go
+++ main.go
@@ -2,9 +2,7 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"runtime"
 	"strings"
 	"time"
 
@@ -37,9 +35,6 @@ func main() {
 	pullConcurrencyLevel := 5
 	getDataFromBugzilla(conn, pullConcurrencyLevel)
 
-	fmt.Println("getDataFromBugzilla finished")
-	printMemUsage()
-
 	err = sqlitex.ExecuteTransient(conn, "SELECT count(id) from bugs;", &sqlitex.ExecOptions{
 		ResultFunc: func(stmt *sqlite.Stmt) error {
 			fmt.Println("totalBugs", stmt.ColumnText(0))
@@ -52,11 +47,9 @@ func main() {
 }
 
 func getDataFromBugzilla(conn *sqlite.Conn, pullConcurrencyLevel int) {
-	printMemUsage()
-
 	tn := time.Now().UTC()
 	fmt.Println("Getting data from Bugzilla start", tn.String())
-	defer func() { fmt.Println("finished in", time.Since(tn)) }()
+	defer func() { fmt.Println("getDataFromBugzilla finished in", time.Since(tn)) }()
 
 	chanDone := make(chan struct{})
 	chanBug := make(chan bugz.Bug, 300*1000)
@@ -68,7 +61,6 @@ func getDataFromBugzilla(conn *sqlite.Conn, pullConcurrencyLevel int) {
 		fmt.Println("got bugs in", time.Since(tn))
 		time.Sleep(1 * time.Minute) // wait a bit for all http clients read data and put it into chanBug
 		close(chanBug)
-		printMemUsage()
 	}()
 
 	var i int
@@ -188,58 +180,44 @@ func createTables(conn *sqlite.Conn) {
 	CheckFatal(err)
 }
 
-func getPhabricatorData(db *sql.DB) {
+func getPhabricatorData(conn *sqlite.Conn) {
 	fmt.Println("Getting data from Phabricator")
 	phab := phab.NewPhabClient()
 	users := phab.UserSearch().Get()
 
-	tx, err := db.Begin() // Begin a transaction
-	CheckFatal(err)
-	stmt, err := tx.Prepare(`
-    INSERT INTO phab_users (
-        id,
-        type,
-        phid,
-        username,
-        real_name,
-        date_created,
-        date_modified,
-        roles,
-        policy_edit,
-        policy_view
-    )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-`)
-	CheckFatal(err)
-	defer stmt.Close()
-
 	for i, user := range users {
 		fmt.Printf("\rInserting phab user into sql: %d", i)
-		_, err = stmt.Exec(
-			user.ID,
-			user.Type,
-			user.Phid,
-			user.Fields.Username,
-			user.Fields.RealName,
-			user.Fields.DateCreated,
-			user.Fields.DateModified,
-			strings.Join(user.Fields.Roles, ","),
-			user.Fields.Policy.Edit,
-			user.Fields.Policy.View,
-		)
-		if err != nil {
-			fmt.Printf("Inserting phab user into sql %v, err: %s", i, err)
-		}
-	}
-	fmt.Println()
-	tx.Commit()
-}
 
-func printMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	fmt.Printf("\nAlloc = %v MiB", m.Alloc/1024/1024)
-	fmt.Printf("\tTotalAlloc = %v MiB", m.TotalAlloc/1024/1024)
-	fmt.Printf("\tSys = %v MiB", m.Sys/1024/1024)
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+		err := sqlitex.ExecuteTransient(
+			conn,
+			`  INSERT INTO phab_users (
+				id,
+				type,
+				phid,
+				username,
+				real_name,
+				date_created,
+				date_modified,
+				roles,
+				policy_edit,
+				policy_view
+			)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+			&sqlitex.ExecOptions{
+				Args: []any{
+					user.ID,
+					user.Type,
+					user.Phid,
+					user.Fields.Username,
+					user.Fields.RealName,
+					user.Fields.DateCreated,
+					user.Fields.DateModified,
+					strings.Join(user.Fields.Roles, ","),
+					user.Fields.Policy.Edit,
+					user.Fields.Policy.View,
+				},
+			},
+		)
+		CheckFatal(err)
+	}
 }
