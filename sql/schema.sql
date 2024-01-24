07b64fe4363f21a78d4dc01e887fcb802589a052
Ihor Antonov <ihor@antonovs.family>
2024-01-24 11:04:42 -0800

    sql graph schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,13 +1,52 @@
-CREATE TABLE IF NOT EXISTS phab_users 
-(
-    id              INTEGER PRIMARY KEY,
-    type            TEXT,
-    phid            TEXT,
-    username        TEXT,
-    real_name       TEXT,
-    date_created    INTEGER,
-    date_modified   INTEGER,
-    roles           TEXT,
-    policy_edit     TEXT,
-    policy_view     TEXT
+PRAGMA foreign_keys = ON;
+
+-- Global object registry is needed to enforce correctness of
+-- relationships. I.e. to have cascade deletes and other sql goodies
+-- also holds original json values obtained from APIs
+CREATE TABLE IF NOT EXISTS nodes (
+    id        TEXT PRIMARY KEY,
+    type      TEXT,
+    eid       INTEGER,         -- eintity id
+    raw       TEXT,            -- json
+    UNIQUE (type, eid)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id        TEXT PRIMARY KEY REFERENCES nodes(id),
+    email     TEXT NOT NULL,
+    real_name TEXT,
+    name      TEXT
+);
+
+CREATE TABLE IF NOT EXISTS bugs (
+    id         TEXT PRIMARY KEY REFERENCES nodes(id),
+    summary    TEXT,
+    created_at TEXT,
+    updated_at TEXT
+);
+
+-- Label is a key-value property of a node.
+-- Lables do not carry realationship information
+CREATE TABLE IF NOT EXISTS labels (
+    obj TEXT,
+    key TEXT,
+    val TEXT, -- json
+    PRIMARY KEY (obj, key),
+    FOREIGN KEY (obj) REFERENCES nodes(id)
+);
+
+CREATE TABLE IF NOT EXISTS edges (
+    id   INTEGER PRIMARY KEY,
+    name TEXT,
+    data TEXT -- json
+);
+
+CREATE TABLE IF NOT EXISTS edge_data (
+    rel INTEGER,
+    obj TEXT,
+    idx INTEGER,
+    PRIMARY KEY (rel, obj),
+    FOREIGN KEY (rel) REFERENCES edges(id)
+    FOREIGN KEY (obj) REFERENCES nodes(id),
+    UNIQUE (rel, obj)
 );
