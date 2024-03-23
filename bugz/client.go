a0c0662d4fbbe0a8a70d8b7a735e8b2244be6965
Xeonid <alkoval11@gmail.com>
2024-03-23 03:46:29 -0700

    Update client.go

--- bugz/client.go
+++ bugz/client.go
@@ -12,8 +12,8 @@ import (
 )
 
 type BugzClient struct {
-	token string
-	url   string
+	Token string
+	URL   string
 
 	http *http.Client
 }
@@ -23,29 +23,24 @@ type BugzLoginResponse struct {
 	Token string `json:"token"`
 }
 
-func NewBugzClient() BugzClient {
-	login, ok := os.LookupEnv("BUGZILLA_LOGIN")
-	if !ok {
-		panic("BUGZILLA_LOGIN is not set")
+func NewBugzClient() *BugzClient {
+	login := os.Getenv("BUGZILLA_LOGIN") //Retrieve env var values and check if they are empty
+	password := os.Getenv("BUGZILLA_PASSWORD")
+	if login == "" || password == "" {
+		panic("BUGZILLA_LOGIN or BUGZILLA_PASSWORD is not set")
 	}
-	password, ok := os.LookupEnv("BUGZILLA_PASSWORD")
-	if !ok {
-		panic("BUGZILLA_PASSWORD is not set")
-	}
-	var loginResponse BugzLoginResponse
 
-	bugz := BugzClient{
-		url:   "https://bugs.freebsd.org/bugzilla/rest",
-		token: "",
-		http:  &http.Client{
-			// Timeout: time.Second * 10,
-		},
+	bugz := &BugzClient{
+		URL:   "https://bugs.freebsd.org/bugzilla/rest",
+		Token: "",
+		http:  &http.Client{},
 	}
+
 	formData := url.Values{}
 	formData.Set("login", login)
 	formData.Set("password", password)
 
-	response, err := bugz.http.Get(bugz.url + "/login?" + formData.Encode())
+	response, err := bugz.http.Get(bugz.URL + "/login?" + formData.Encode())
 	CheckFatal("NewBugzClient bugz.http.Get", err)
 	defer response.Body.Close()
 
@@ -53,15 +48,40 @@ func NewBugzClient() BugzClient {
 		CheckFatal("NewBugzClient", fmt.Errorf("login failed, status code: %d", response.StatusCode))
 	}
 
+	var loginResponse BugzLoginResponse
 	if err := json.NewDecoder(response.Body).Decode(&loginResponse); err != nil {
 		CheckFatal("NewBugzClient", fmt.Errorf("error reading bugzilla login response body: %w", err))
 	}
 
 	if loginResponse.Token == "" {
-		err = fmt.Errorf("login token is empty")
-		CheckFatal("NewBugzClient loginResponse.Token", err)
+		CheckFatal("NewBugzClient loginResponse.Token", fmt.Errorf("login token is empty"))
 	}
-	bugz.token = loginResponse.Token
+	bugz.Token = loginResponse.Token
 
 	return bugz
+}
+
+func (bc *BugzClient) GetToken() string {
+	return bc.Token
+}
+
+func (bc *BugzClient) DownloadBugs() ([]Bug, error) {
+	// Make a GET request to fetch bugs
+	response, err := bc.http.Get(bc.URL + "/bugs")
+	if err != nil {
+		return nil, fmt.Errorf("error making GET request to Bugzilla API: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	// Decode the response body into a slice of Bug objects
+	var bugs []Bug
+	if err := json.NewDecoder(response.Body).Decode(&bugs); err != nil {
+		return nil, fmt.Errorf("error decoding JSON response: %w", err)
+	}
+
+	return bugs, nil
 }
