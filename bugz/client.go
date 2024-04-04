e8664aa93ec155989c9cdd643f86065ab78524be
Xeonid <alkoval11@gmail.com>
2024-04-04 12:53:52 -0700

    Update client.go

--- bugz/client.go
+++ bugz/client.go
@@ -26,34 +26,7 @@ type BugzLoginResponse struct {
 }
 
 func NewBugzClient() *BugzClient {
-	//login := os.Getenv("BUGZILLA_LOGIN") //Retrieve env var values and check if they are empty
-	//password := os.Getenv("BUGZILLA_PASSWORD")
-	//if login == "" || password == "" {
-	//	panic("BUGZILLA_LOGIN or BUGZILLA_PASSWORD is not set")
-	//}
-
-	/*formData := url.Values{}
-	formData.Set("login", login)
-	formData.Set("password", password)
-
-	response, _ := bc.http.Get(bc.URL + "/login?" + formData.Encode())
-	//CheckFatal("NewBugzClient bc.http.Get", err)
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		CheckFatal("NewBugzClient", fmt.Errorf("login failed, status code: %d", response.StatusCode))
-	}
-
-	var loginResponse BugzLoginResponse
-	if err := json.NewDecoder(response.Body).Decode(&loginResponse); err != nil {
-		CheckFatal("NewBugzClient", fmt.Errorf("error reading bugzilla login response body: %w", err))
-	}
-
-	if loginResponse.Token == "" {
-		CheckFatal("NewBugzClient loginResponse.Token", fmt.Errorf("login token is empty"))
-	}
-	bc.Token = loginResponse.Token*/
-
+	
 	return &BugzClient{
 		URL:   "https://bugs.freebsd.org/bugzilla/rest",
 		token: "",
