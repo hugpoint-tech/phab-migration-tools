83d1c4856cfd011d794c9aceaace693966adfe00
Xeonid <alkoval11@gmail.com>
2024-04-08 21:14:53 -0700

    Update bugz_test.go

--- bugz/bugz_test.go
+++ bugz/bugz_test.go
@@ -3,26 +3,35 @@ package bugz
 import "testing"
 
 func TestExtractIDs(t *testing.T) {
-
+	
 	bug := Bug{
 		AssignedToDetail: User{ID: 101},
 		CCDetail:         []User{{ID: 102}, {ID: 103}},
 		CreatorDetail:    User{ID: 104},
 	}
-
-	ids := extractIDs(bug)
-
+	
 	expectedIDs := map[int]User{
 		101: {ID: 101},
 		102: {ID: 102},
 		103: {ID: 103},
 		104: {ID: 104},
 	}
+	
+	actualIDs := extractIDs(bug)
+	
+	if len(actualIDs) != len(expectedIDs) {
+		t.Errorf("Number of actual IDs does not match number of expected IDs")
+		return
+	}
 
-
-	for id, user := range expectedIDs {
-		if ids[id].ID != user.ID {
-			t.Errorf("extractIDs returned incorrect ID for user %d", id)
+	for id, expectedUser := range expectedIDs {
+		actualUser, ok := actualIDs[id]
+		if !ok {
+			t.Errorf("Expected ID %d not found in actual IDs", id)
+			return
+		}
+		if actualUser.ID != expectedUser.ID {
+			t.Errorf("Actual user ID %d does not match expected user ID %d for ID %d", actualUser.ID, expectedUser.ID, id)
 		}
 	}
 }
