1723e56ec007652632c58c3eb6b812dea2173165
Xeonid <alkoval11@gmail.com>
2024-05-13 19:35:59 -0700

    Update db_test.go

--- bugz/db_test.go
+++ bugz/db_test.go
@@ -12,34 +12,34 @@ func TestCreateAndInitializeDatabase(t *testing.T) {
 	defer db.Close()
 	
 	// Query the bugs table to verify that the sample data was inserted correctly
-	stmt, err := db.Prepare(`SELECT id, CreationTime, Creator, Summary, OtherFieldsJSON FROM bugs WHERE id = ?`)
+	selectQuery := "SELECT id, CreationTime, Creator, Summary, OtherFieldsJSON FROM bugs WHERE id = ?"
+	err = sqlitex.ExecuteTransient(db, selectQuery, func(stmt *sqlite.Stmt) error {
+		var (
+			id              int64
+			creationTime    string
+			creator         string
+			summary         string
+			otherFieldsJSON string
+		)
+
+		if stmt.Step() {
+			id = stmt.ColumnInt64(0)
+			creationTime = stmt.ColumnText(1)
+			creator = stmt.ColumnText(2)
+			summary = stmt.ColumnText(3)
+			otherFieldsJSON = stmt.ColumnText(4)
+		} else {
+			return fmt.Errorf("No rows found for id = 1")
+		}
+
+		// Verify the retrieved data matches the sample data
+		if id != 1 || creationTime != "2077-10-23 09:42:00" || creator != "John Dead" || summary != "Sample Bug" || otherFieldsJSON != `{"key": "value"}` {
+			t.Fatalf("Retrieved data does not match sample data")
+		}
+
+		return nil
+	}, 1)
 	if err != nil {
-		t.Fatalf("Failed to prepare statement: %v", err)
-	}
-	defer stmt.Finalize()
-
-	hasRow, err := stmt.Step()
-	if err != nil {
-		t.Fatalf("Error retrieving data: %v", err)
-	} else if !hasRow {
-		t.Fatalf("No rows found for id = 1")
-	}
-
-	var (
-		id             int64
-		creationTime   string
-		creator        string
-		summary        string
-		otherFieldsJSON string
-	)
-	id = stmt.ColumnInt64(0)
-	creationTime = stmt.ColumnText(1)
-	creator = stmt.ColumnText(2)
-	summary = stmt.ColumnText(3)
-	otherFieldsJSON = stmt.ColumnText(4)
-
-	// Verify the retrieved data matches the sample data
-	if id != 1 || creationTime != "2077-10-23 09:42:00" || creator != "John Dead" || summary != "Sample Bug" || otherFieldsJSON != `{"key": "value"}` {
-		t.Fatalf("Retrieved data does not match sample data")
+		t.Fatalf("Error executing select statement: %v", err)
 	}
 }
