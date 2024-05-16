4e73bf4709c2b6e0289f696c96ddbd76d7df6d1e
Xeonid <alkoval11@gmail.com>
2024-05-16 15:05:19 -0700

    Update db_test.go

--- bugz/db_test.go
+++ bugz/db_test.go
@@ -16,11 +16,9 @@ func TestCreateAndInitializeDatabase(t *testing.T) {
 
 	// Insert sample data into the bugs table
 	insertText := `INSERT INTO bugs (id, CreationTime, Creator, Summary, OtherFieldsJSON) VALUES (1, '2077-10-23 09:42:00', 'John Dead', 'Sample Bug', '{}')`
-
-	err := sqlitex.ExecuteTransient(db, insertText, nil)
-
-	if err != nil {
-    	    t.Fatalf("Error executing insert statement: %v", err)
+	
+	if err := sqlitex.ExecuteTransient(db, insertText, nil); err != nil {
+		t.Fatalf("Error executing insert statement: %v", err)
 	}
 
 	// Prepare the statement for querying the bugs table
