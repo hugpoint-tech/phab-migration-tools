841fcc54f8fb45b537a4b6b8d06d67fa1c7b7ba2
Ihor Antonov <ihor@antonovs.family>
2024-09-14 11:34:12 -0700

    bugz: remove unused functions

--- bugz/client.go
+++ bugz/client.go
@@ -11,8 +11,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 type BugzClient struct {
@@ -120,17 +118,6 @@ func (bc *BugzClient) DownloadBugzillaBugs() ([]Bug, error) { // Make URL to bug
 	return bugs, nil
 }
 
-func writeToFile(filename string, bug Bug) error {
-	// Use json.MarshalIndent to preserve the original formatting
-	indentedData, err := json.MarshalIndent(bug, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	// Write the data to the specified file
-	return os.WriteFile(filename, indentedData, 0644)
-}
-
 func (bc *BugzClient) ShowBugs() error {
 	fmt.Println("showing bugs")
 	return nil
@@ -139,77 +126,6 @@ func (bc *BugzClient) ShowBugs() error {
 func (bc *BugzClient) ListBugs() error {
 	fmt.Println("listing bugs")
 	return nil
-}
-
-func getIDS() (map[int]User, error) {
-	// Specify the path to the directory containing JSON files
-	directoryPath := "bugs"
-
-	fmt.Println("Get IDS from bugs")
-
-	// Get a list of all JSON files in the directory
-	files, err := os.ReadDir(directoryPath)
-	if err != nil {
-		return nil, fmt.Errorf("error reading directory: %w", err)
-	}
-
-	// Map to store extracted IDs and corresponding User objects
-	idUserMap := make(map[int]User)
-
-	// Iterate over each file in the directory
-	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".json") {
-			// Construct the full path to the JSON file
-			filePath := filepath.Join(directoryPath, file.Name())
-
-			// Read the JSON file
-			jsonData, err := os.ReadFile(filePath)
-			if err != nil {
-				fmt.Printf("Error reading JSON file %s: %v\n", filePath, err)
-				continue
-			}
-
-			// Process the JSON data
-			var bug Bug
-			err = json.Unmarshal(jsonData, &bug)
-			if err != nil {
-				fmt.Printf("Error decoding JSON from file %s: %v\n", filePath, err)
-				continue
-			}
-
-			// Add the extracted IDs to the map
-			ids := extractIDs(bug)
-			for id, user := range ids {
-				idUserMap[id] = user
-			}
-		}
-	}
-
-	return idUserMap, nil
-}
-
-func extractIDs(bug Bug) map[int]User {
-	// Create a map to store the unique IDs and corresponding Users
-	idUserMap := make(map[int]User)
-
-	// Extract 'id' field from AssignedToDetail
-	if _, ok := idUserMap[bug.AssignedToDetail.ID]; !ok {
-		idUserMap[bug.AssignedToDetail.ID] = bug.AssignedToDetail
-	}
-
-	// Extract 'id' field from CCDetail
-	for _, ccUser := range bug.CCDetail {
-		if _, ok := idUserMap[ccUser.ID]; !ok {
-			idUserMap[ccUser.ID] = ccUser
-		}
-	}
-
-	// Extract 'id' field from CreatorDetail
-	if _, ok := idUserMap[bug.CreatorDetail.ID]; !ok {
-		idUserMap[bug.CreatorDetail.ID] = bug.CreatorDetail
-	}
-
-	return idUserMap
 }
 
 func (bc *BugzClient) DownloadBugComments(bugID int64) ([]Comment, error) {
